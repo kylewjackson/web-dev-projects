8c547b26ecdc61d3e95e9945b2b27f4466f79038
Kyle Jackson <kylewalkerjackson@gmail.com>
2019-06-17 01:23:07 -0500

    Still working on todo list, but I've commented out functionality that I haven't been able to figure out yet. Hopefully I'll be able to implement soon.

--- Udemy/Web Developer Bootcamp/Javascript Projects/jQuery Projects/to-do-list/assets/js/todo.js
+++ Udemy/Web Developer Bootcamp/Javascript Projects/jQuery Projects/to-do-list/assets/js/todo.js
@@ -31,8 +31,24 @@ const alpha = () => $("li").sort((a, b) =>
 	$(b).text().charAt(1).toUpperCase() < $(a).text().charAt(1).toUpperCase() ? 1 : -1
 	).appendTo('#todo-list');
 
+$('#az').on('click', function(){
+	alpha();
+})
+
 //show sort options
 $('.sort').on('click', function(){
-	$('#options').toggleClass('btwn');;
-	$('.sort-kind').toggleClass('hide');
+	$(this).toggleClass('sort-clicked');;
+	$('.sort-kind').toggleClass('sort-vis');
 })
+
+function tester(ele, undo) {
+	if (undo) {
+		$(ele).removeAttr('style');
+	} else {
+		$(ele).css('color', 'white');
+		$(ele).css('background-color', 'red');
+		$(ele).css('border', '1px solid black')
+	}
+}
+
+
