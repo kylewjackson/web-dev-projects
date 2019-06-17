9a2158e1653ae6b22ae2d674b29e666bb72391ef
Kyle Jackson <kylewalkerjackson@gmail.com>
2019-06-17 04:12:28 -0500

    Got a lot of the functionality of the todo list working, including pinning/favoriting, alphabetizing, and the sort option menu.

--- Udemy/Web Developer Bootcamp/Javascript Projects/jQuery Projects/to-do-list/assets/js/todo.js
+++ Udemy/Web Developer Bootcamp/Javascript Projects/jQuery Projects/to-do-list/assets/js/todo.js
@@ -1,3 +1,5 @@
+let sortMode = 'none';
+
 // Check off specifc todos by clicking
 $('ul').on('click', 'li', function (){
 	$(this).toggleClass('completed');
@@ -17,28 +19,75 @@ $('input[type="text"]').keypress(function(e){
 		let todoText = $(this).val();
 		$(this).val('');
 		//create new li to add to ul
-		$('ul').append(`<li><span><i class="fas fa-trash-alt"></i></span> ${todoText}</li>`);
+		$('ul').append(`<li><span class="del"><i class="fas fa-trash-alt"></i></span>${todoText}<span class="fav"><i class="fas fa-star"></i></span></li>`);
+		if (sortMode === 'az') {
+			alpha();
+		}
 	}
 });
 
 //fade input field
 $('span').eq(0).click(function(){
 	$('input[type="text"]').fadeToggle(200);
+	$('h1 i').toggleClass('fa-pen-square').toggleClass('fa-minus');;
+})
+
+//mark as prioirity
+$('ul').on('click', '.fav', function(e){
+	if (!$(this).hasClass('active')) {
+		$(this).parent().prependTo('#todo-list');
+	}
+	$(this).toggleClass('active');
+	if (sortMode === 'az') {
+		alpha();
+	}
+	e.stopPropagation();
 })
 
 //alphabetize list
-const alpha = () => $("li").sort((a, b) => 
-	$(b).text().charAt(1).toUpperCase() < $(a).text().charAt(1).toUpperCase() ? 1 : -1
+const alpha = () => $(".fav:not(.active)").parent().sort((a, b) => 
+	$(b).text().toUpperCase() < $(a).text().toUpperCase() ? 1 : -1
 	).appendTo('#todo-list');
 
 $('#az').on('click', function(){
 	alpha();
+	sortMode = 'az';
+})
+
+//other list options
+$('#asc').on('click', function(){
+	sortMode = 'asc';
+})
+
+$('#desc').on('click', function(){
+	sortMode = 'desc';
 })
 
 //show sort options
 $('.sort').on('click', function(){
 	$(this).toggleClass('sort-clicked');;
 	$('.sort-kind').toggleClass('sort-vis');
+})
+
+//set sort mode
+$('#options').on('click', function(e){
+	if ($(e.target).hasClass('active') || $(e.target).hasClass('sort')) {
+		$(e.target).toggleClass('active');
+	} else {
+		if (sortMode === 'az') {
+			$('#az').addClass('active');
+			$('#asc').removeClass('active');
+			$('#desc').removeClass('active');
+		} else if (sortMode === 'asc') {
+			$('#az').removeClass('active');
+			$('#asc').addClass('active');
+			$('#desc').removeClass('active');
+		} else if (sortMode === 'desc') {
+			$('#az').removeClass('active');
+			$('#asc').removeClass('active');
+			$('#desc').addClass('active');		
+		}
+	}
 })
 
 function tester(ele, undo) {
