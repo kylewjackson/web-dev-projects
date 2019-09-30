118f34522f0a5ea09c4fafccb1ef98794490ffed
Kyle Jackson <kylewalkerjackson@gmail.com>
2019-09-30 01:33:46 -0500

    changing relative path

---
+++ React-Product-Page/precache-manifest.c7a9b67d34f20922b786b5cbc8ad07da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e509afed454aaf33a6f5d85a6b06a07c",
+    "url": "/react-product-page/index.html"
+  },
+  {
+    "revision": "2c9804978015ed68d8c9",
+    "url": "/react-product-page/static/css/main.49a554fd.chunk.css"
+  },
+  {
+    "revision": "477a2fdb3481c55e6cba",
+    "url": "/react-product-page/static/js/2.a3a2298d.chunk.js"
+  },
+  {
+    "revision": "2c9804978015ed68d8c9",
+    "url": "/react-product-page/static/js/main.7a117c3e.chunk.js"
+  },
+  {
+    "revision": "532021839e67c95efb7a",
+    "url": "/react-product-page/static/js/runtime~main.296e7352.js"
+  }
+]);
--- React-Product-Page/service-worker.js
+++ React-Product-Page/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b37e3025ccb16556d710c7638f1e9fa5.js"
+  "/react-product-page/precache-manifest.c7a9b67d34f20922b786b5cbc8ad07da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-product-page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
