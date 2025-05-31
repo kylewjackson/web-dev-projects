b7025a1fe2ca9f6e592fb5a74bdec2d1a0cf0efa
Mona Lisa <kylewalkerjackson@gmail.com>
2025-05-31 02:03:48 -0500

    scss structure and setup for tokens

--- React-Product-Demo/product-page-demo/vite.config.ts
+++ React-Product-Demo/product-page-demo/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': '/src',
+			"@styles": '/src/styles',
+			"@variables": '/src/styles/variables/_index.scss',
+			"@mixins": '/src/styles/mixins/_index.scss',
+			"@index": "/src/styles/_index.scss"
+		}
+	}
 })
