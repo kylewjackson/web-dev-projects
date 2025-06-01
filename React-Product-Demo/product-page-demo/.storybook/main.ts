b2b5c4244aee1d14cc9ed50c96cad8a80aef862f
Mona Lisa <kylewalkerjackson@gmail.com>
2025-05-31 20:16:39 -0500

    svgr for vite

--- React-Product-Demo/product-page-demo/.storybook/main.ts
+++ React-Product-Demo/product-page-demo/.storybook/main.ts
@@ -1,21 +1,27 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import svgr from 'vite-plugin-svgr';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
-	"storybook-addon-pseudo-states"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+	"stories": [
+		"../src/**/*.mdx",
+		"../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+	],
+	"addons": [
+		"@storybook/addon-onboarding",
+		"@chromatic-com/storybook",
+		"@storybook/addon-docs",
+		"@storybook/addon-a11y",
+		"@storybook/addon-vitest",
+		"storybook-addon-pseudo-states"
+	],
+	"framework": {
+		"name": "@storybook/react-vite",
+		"options": {}
+	},
+	async viteFinal(config) {
+		config.plugins = config.plugins || [];
+		config.plugins.push(svgr());
+		return config;
+	},
 };
 export default config;
