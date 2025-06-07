19945c8229c4dca3367375283f007ac745571bbd
Mona Lisa <kylewalkerjackson@gmail.com>
2025-06-07 10:46:44 -0500

    project setup with vite, react, typescript, bootstrap, and prettier config

---
+++ Movie-Project/src/App.tsx
@@ -0,0 +1,14 @@
+// import './App.css'
+
+function App() {
+  return (
+    <div className="container py-4">
+      <header className="text-center mb-2">
+        <h1>Movie Watchlist</h1>
+      </header>
+      <main>{/* Content */}</main>
+    </div>
+  );
+}
+
+export default App;
