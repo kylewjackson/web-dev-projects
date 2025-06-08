1c0a758e399682e9da12deb51502623628306921
Mona Lisa <kylewalkerjackson@gmail.com>
2025-06-07 20:48:11 -0500

    Basic UI #32 search bar

--- Movie-Project/src/App.tsx
+++ Movie-Project/src/App.tsx
@@ -1,12 +1,24 @@
 // import './App.css'
 
+import SearchBar from "./components/SearchBar";
+
+function Main() {
+  return (
+    <main className="row justify-content-center">
+      <section className="col-9 col-lg-6">
+        <SearchBar onSearch={(query) => console.log("Searching for:", query)} />
+      </section>
+    </main>
+  );
+}
+
 function App() {
   return (
     <div className="container py-4">
       <header className="text-center mb-2">
         <h1>Movie Watchlist</h1>
       </header>
-      <main>{/* Content */}</main>
+      <Main />
     </div>
   );
 }
---
+++ Movie-Project/src/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+import { useId, useState } from "react";
+
+type Props = {
+	onSearch: (query: string) => void;
+}
+
+export default function SearchBar({onSearch}: Props) {
+  const [query, setQuery] = useState("");
+  const searchId = useId();
+
+  function handleSearch(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formattedQuery = query.trim();
+    onSearch(formattedQuery);
+  }
+
+  return (
+    <form onSubmit={handleSearch} className="row gx-1">
+      <div className="form-floating col-9">
+        <input
+          id={searchId}
+          type="search"
+          name="searchQuery"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder=" " //For Bootstrap floating form
+          className="form-control"
+        />
+        <label htmlFor={searchId}>Search Movies:</label>
+      </div>
+      <button type="submit" className="col-3 btn btn-primary" disabled={!query.trim()}>
+          Search
+        </button>
+    </form>
+  );
+}
