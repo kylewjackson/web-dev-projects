07213aeaa98d82da94b7235e80936cc6dd4f4f96
Mona Lisa <kylewalkerjackson@gmail.com>
2025-06-14 14:05:29 -0500

    X2 - Navigation Links #55 feat: replace button navigation with NavLink for improved routing in App component

--- Movie-Project/src/App.tsx
+++ Movie-Project/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route, Navigate, NavLink, Link } from "react-router";
 import type { Movie } from "./types/movie";
 import SearchResults from "./views/SearchResults";
 import WatchlistView from "./views/WatchlistView";
@@ -9,7 +9,6 @@ import "./App.css";
 import Logo from "./assets/logo.svg";
 
 function App() {
-  const [view, setView] = useState("main");
   const [apiLoading, setApiLoading] = useState<boolean>(false);
   const [apiError, setApiError] = useState<Error | null>(null);
   const [ariaMessage, setAriaMessage] = useState<string>("");
@@ -24,17 +23,18 @@ function App() {
       <header className="text-center mb-4">
         <nav className="navbar navbar-expand-sm bg-body-tertiary">
           <div className="container-fluid">
-            <button className="nav-link" onClick={() => setView("main")}>
-              <h1 className="navbar-brand mb-0 d-flex align-items-center">
-                <img
-                  src={Logo}
-                  alt="Movie Watchlist App"
-                  width={30}
-                  height={40}
-                />
-                <p className="d-sm-none m-0 ps-3">Watchlist App</p>
-              </h1>
-            </button>
+            <Link
+              to="/"
+              className="navbar-brand mb-0 d-flex align-items-center"
+            >
+              <img
+                src={Logo}
+                alt="Movie Watchlist App Logo"
+                width={30}
+                height={40}
+              />
+              <p className="d-sm-none m-0 ps-3">Watchlist App</p>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -48,22 +48,17 @@ function App() {
             </button>
             <div className="collapse navbar-collapse" id="Navbar">
               <ul className="navbar-nav">
-                <button
-                  className={`nav-link ${view === "main" && "active"}`}
-                  aria-current={view === "main" && "page"}
-                  disabled={view === "main" && true}
-                  onClick={() => setView("main")}
-                >
-                  Search
-                </button>
-                <button
-                  className={`nav-link ${view === "watchlist" && "active"}`}
-                  aria-current={view === "watchlist" && "page"}
-                  onClick={() => setView("watchlist")}
-                  disabled={view === "watchlist" && true}
-                >
-                  Watchlist
-                </button>
+                <li className="nav-item">
+                  <NavLink to="/search" className="nav-link">
+                    Search
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/watchlist" className="nav-link">
+                    <span>Watchlist</span>
+                    <span> ({watchlist && watchlist.length})</span>
+                  </NavLink>
+                </li>
               </ul>
             </div>
           </div>
